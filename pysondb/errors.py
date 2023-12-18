11afe23e8a5adb22cb382cd7ac582bf8ebce5a3b
bl1nc <bl1nch.pro@gmail.com>
2023-12-19 01:13:59 +0600

    Unsafe custom id feature

--- pysondb/errors.py
+++ pysondb/errors.py
@@ -22,3 +22,11 @@ class IdDoesNotExistError(Exception):
 
     def __str__(self) -> str:
         return str(self.message)
+
+
+class UnsafeCustomIdError(Exception):
+    def __init__(self, message: str) -> None:
+        self.message = message
+
+    def __str__(self) -> str:
+        return str(self.message)
